f1ece874a6f70867bc11608af6db16fb82ffa64f
Comnarin <Nsirinapuk@gmail.com>
2025-03-20 02:15:52 +0700

    Update calculate.go

--- Handlers/calculate.go
+++ Handlers/calculate.go
@@ -7,20 +7,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func CalculateDiscount2(c *fiber.Ctx) error {
-	return c.SendString("Hello, World!")
-}
-func CalculateDiscount3(c *fiber.Ctx) error {
-	var req requests.Cart
-
-	if err := c.BodyParser(&req); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(err)
-	}
-
-	total := services.ApplyDiscount(req)
-
-	return c.JSON(fiber.Map{"total": total})
-}
 func CalculateDiscount(c *fiber.Ctx) error {
 	var req requests.Cart
 
